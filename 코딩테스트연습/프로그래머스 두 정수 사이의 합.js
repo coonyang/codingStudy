502ecfc32d9355d67fad9e69c9ecb2fafdb01c95
Hajun Jeon <gkwns1231@naver.com>
2026-06-16 19:16:41 +0900

    도장깨기 12

---
+++ 코딩테스트연습/프로그래머스 두 정수 사이의 합.js
@@ -0,0 +1,10 @@
+function solution(a, b) {
+  var answer = 0;
+  let arr = [a, b];
+  arr.sort((a, b) => a - b);
+  for (let i = arr[0]; i <= arr[1]; i++) {
+    answer = answer + i;
+  }
+
+  return answer;
+}
