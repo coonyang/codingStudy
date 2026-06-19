902ab9a67e21779d8c53e1e6c25a9a2d33392045
Hajun Jeon <gkwns1231@naver.com>
2026-06-19 20:24:11 +0900

    도장깨기 23

---
+++ 코딩테스트연습/프로그래머스 수박수박수박수박수박수.js
@@ -0,0 +1,18 @@
+function solution(n) {
+  var answer = "";
+
+  for (let i = 0; i < n; i++) {
+    if (i % 2 === 0) {
+      answer += "수";
+    } else {
+      answer += "박";
+    }
+  }
+
+  return answer;
+}
+
+// 다른 정답
+function solution(n) {
+  return "수박".repeat(n).slice(0, n);
+}
