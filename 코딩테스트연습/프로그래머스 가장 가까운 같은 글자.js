ac4c0dc8316bb4b58020ef6a54c6e442e302e4e7
Hajun Jeon <gkwns1231@naver.com>
2026-06-23 19:31:01 +0900

    도장깨기 38

---
+++ 코딩테스트연습/프로그래머스 가장 가까운 같은 글자.js
@@ -0,0 +1,17 @@
+function solution(s) {
+  var answer = [];
+
+  let obj = {};
+
+  for (let i = 0; i < s.length; i++) {
+    if (obj[s[i]] === undefined) {
+      answer.push(-1);
+      obj[s[i]] = i;
+    } else {
+      answer.push(i - obj[s[i]]);
+      obj[s[i]] = i;
+    }
+  }
+
+  return answer;
+}
