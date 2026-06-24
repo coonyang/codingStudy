04ca66c51fc77677bbe20f031e84e129a28919d5
Hajun Jeon <gkwns1231@naver.com>
2026-06-25 04:10:01 +0900

    풀었던 문제가 나와 + 정보

--- 코딩테스트연습/프로그래머스 명예의 전당(1) 문제.js
+++ 코딩테스트연습/프로그래머스 명예의 전당(1) 문제.js
@@ -6,7 +6,7 @@ function solution(k, score) {
   for (let i = 0; i < score.length; i++) {
     arr.push(score[i]);
     arr.sort((a, b) => b - a);
-    arr = arr.slice(0, k);
+    arr = arr.slice(0, k); // 하나 지울땐 shift() 로 제거 가능
     answer.push(arr[arr.length - 1]);
   }
 
