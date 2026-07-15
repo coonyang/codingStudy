4bf28da35b33ecef4044367f6e9efc557321362b
Hajun Jeon <gkwns1231@naver.com>
2026-07-15 17:23:07 +0900

    도장깨기 79

---
+++ 코딩테스트연습/프로그래머스 공백으로 구분하기1 2.js
@@ -0,0 +1,15 @@
+function solution(my_string) {
+  var answer = [];
+
+  answer = my_string.split(" ");
+
+  return answer;
+}
+
+function solution(my_string) {
+  var answer = [];
+
+  answer = my_string.trim().split(/\s+/);
+
+  return answer;
+}
