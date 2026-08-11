442ff2623471c90077a85fdb5f14fbffedc9f6b4
Hajun Jeon <gkwns1231@naver.com>
2026-08-12 06:18:56 +0900

    102 수정

--- 코딩테스트연습/프로그래머스 행렬의 곱셈.js
+++ 코딩테스트연습/프로그래머스 행렬의 곱셈.js
@@ -1,8 +1,12 @@
 function solution(arr1, arr2) {
   var answer = [[]];
 
+  for (let i = 1; i < arr1.length; i++) {
+    answer.push([]);
+  }
+
   for (let i = 0; i < arr1.length; i++) {
-    for (let k = 0; k < arr2.length; k++) {
+    for (let k = 0; k < arr2[0].length; k++) {
       let a = 0;
       for (let j = 0; j < arr1[i].length; j++) {
         let count = arr1[i][j] * arr2[j][k];
